7853264851f0f40f6bb7b19855d5c73f73fb2570
SungBin5304 <sungbin.dev@gmail.com>
2020-05-12 17:37:07 +0900

    code cleanup

--- app/src/main/java/com/sungbin/texteditor/MainActivity.kt
+++ app/src/main/java/com/sungbin/texteditor/MainActivity.kt
@@ -1,36 +1,12 @@
 package com.sungbin.texteditor
 
 import android.os.Bundle
-import android.text.SpannableStringBuilder
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        edit.editor.text = SpannableStringBuilder(
-                "1234567890\n" +
-                "1234567890\n" +
-                "1234567890\n" +
-                "1234567890\n" +
-                "1234567890\n" +
-                "1234567890\n" +
-                "1234567890\n" +
-                "1234TesT0\n" +
-                "1234567890\n" +
-                "1234567890"
-        )
-
-        undo.setOnClickListener {
-            edit.undo()
-        }
-
-        redo.setOnClickListener {
-            edit.redo()
-        }
-//        Log.d("AAA - 9", edit.findText("TEST", true)[0][1].toString())
     }
 }
